eb49d4407f69af73a3863d4176778d19fcae1afa
Artem Egorov <Kartpick@gmail.com>
2017-06-13 16:28:50 +0300

    * added twig-template system to view

--- system/view.php
+++ system/view.php
@@ -1,18 +1,25 @@
 <?php namespace System;
 
+use Twig_Loader_Filesystem;
+use Twig_Environment;
 
 class View
 {
   private static $VIEWS_PATH = __DIR__. "/../views/";
   private $className;
+  private $twig;
 
   function __construct($className)
   {
     $this->className = $className;
   }
 
-  function generate($method, $data) {
-    include self::$VIEWS_PATH.$this->className."/".$method.".html";
+  function generate($method, $context) {
+    $loader = new Twig_Loader_Filesystem(self::$VIEWS_PATH);
+    $this->twig = new Twig_Environment($loader);
+
+    //include self::$VIEWS_PATH.$this->className."/".$method.".html";
+    echo $this->twig->render("pages/".$this->className."/".$method.".twig", $context);
   }
 
 }
